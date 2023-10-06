c9d60335e7fe57f16d106644e9d91bf53b9bc544
zakiiiii77 <125500686+zakiiiii77@users.noreply.github.com>
2023-10-06 11:30:58 +0300

    sui

--- app1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import Articles
-
-# Register your models here.
-
-class ArticlesAdmin(admin.ModelAdmin):
-    list_display = ("Article_name","Article_number")
-
-admin.site.register(Articles, ArticlesAdmin)
